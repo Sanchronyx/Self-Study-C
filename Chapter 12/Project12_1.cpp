08e2383850fbc0c750643ba03f8b0fa718c58cc9
Sanchronyx <zlrichards02@gmail.com>
2023-11-20 02:28:16 +0800

    Chapter 12 Continued

--- Chapter 12/Project12_1.cpp
+++ Chapter 12/Project12_1.cpp
@@ -1,33 +1,24 @@
-// #include <stdio.h>
-
-// #define COUNT 1000
-
-
-// int main(void){
-
-//     char sent[COUNT] = {0}, c;
-//     int i = 0, j = 0;
-
-//     while (1){
-//         c = getchar(); 
-//         sent[i] = c;
-//         if (c == '\n')break;
-//         if (sent[i] == COUNT)break;
-//         i++;
-//     }
-
-//     for (j = i-1; j >= 0; j--){
-//         putchar(sent[j]);
-//     }
-
-//     return 0;
-// }
-
 #include <stdio.h>
 
 #define COUNT 1000
 
+
 int main(void){
 
-    
+    char sent[COUNT] = {0}, c;
+    int i = 0, j = 0;
+
+    while (1){
+        c = getchar(); 
+        sent[i] = c;
+        if (c == '\n')break;
+        if (sent[i] == COUNT)break;
+        i++;
+    }
+
+    for (j = i-1; j >= 0; j--){
+        putchar(sent[j]);
+    }
+
+    return 0;
 }
