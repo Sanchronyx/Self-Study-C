2e0da599ef0a3f32b86f0b8e9e959366abae8c58
Sanchronyx <zlrichards02@gmail.com>
2023-12-07 17:46:16 +0800

    JAVA Class Waste Time
    Did another question during java class

--- Chapter 13/Exercises/Exercise13_16_2.c
+++ Chapter 13/Exercises/Exercise13_16_2.c
@@ -6,33 +6,21 @@
 #define LEN 100
 
 bool test_extension (const char *file_name, const char *extension){
-    int i = 0, lenOne = strlen(file_name), lenTwo = strlen(extension);
-    char storage[LEN];
-
-    for (i = 0; i < lenOne && file_name[i] != '.'; i++){
-        if (file_name[i] == '.'){
-            i++;
-            break;
+    int lenOne = strlen(file_name) - 1, lenTwo = strlen(extension) - 1;
+    
+    while (lenOne >= 0 && lenTwo >= 0){
+        if (toupper(file_name[lenOne]) != toupper(extension[lenTwo])) return false;
+        if (lenTwo == 0){
+            if (lenOne > 0) return file_name[lenOne - 1] == '.';
+            else return false;
         }
-        storage[i] = file_name[i];
+        lenOne--;
+        lenTwo--;
     }
-    storage[i] = '\0';
-   
-    char compareArray[LEN];
-    int j = 0, k = 0;
-    for (j = 0, k = i + 1; j < lenTwo; j++, k++){
-        compareArray[j] = file_name[k];
-    }
-    strupr(compareArray);
-    char storageTwo[LEN];
-    for (int l = 0; l < lenTwo; l++){
-        storageTwo[l] = extension[l];
-    }
-    strupr(storageTwo);
-    if (strcmp(compareArray, storageTwo) == 0)return(true);
-    else return (false);
-}
+    
+    return false;
 
+}
 int main(void){
     char inputOne[LEN], inputTwo[LEN], c;
     int i = 0;
